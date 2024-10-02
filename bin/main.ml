535df8b576370907b8f0e6373dbc53c8e48315f7
Dario Pellegrino <dario.pellegrino00@gmail.com>
2024-10-02 13:43:54 +0200

    implemented vim w

--- bin/main.ml
+++ bin/main.ml
@@ -8,7 +8,20 @@ let setup () =
 
 
 (* text buffer definition*)
-let buffer = [| "Hello, world!"; "This is a simple text editor"; "with Vim hjkl movements."; |] 
+let buffer = 
+  [| 
+    "Hello, world!"; 
+    "This is a simple text editor"; 
+    "with Vim hjkl movements."; 
+    "delete all the x to win";
+    "*!**a magnum";
+    "  x   xxx";
+    "x ";
+    "x    x x";
+    "   x ";
+    "              x";
+  |]
+
 let cursor = ref create_cursor
 let font_size = 16  
 let char_width = 8  
@@ -17,58 +30,86 @@ let start_blink_time = ref (get_time ())
 let blink = ref true 
 let blink_delay = 0.5
 
+let cursor_moving = ref false
+
+(* distance of lines from line number*)
+let line_offset = 3
+
+(*draw line numbers*)
+let draw_line_numbers buffer font =
+  Array.iteri(fun i _ ->
+    draw_text_ex font (string_of_int i) (Vector2.create (10. +. float_of_int (0 * char_width)) (10. +. float_of_int (i * char_height))) (float_of_int font_size) 1.0 Color.yellow; 
+  ) buffer
+
 (* buffer visualization*)
 let draw_buffer buffer cursor font blink =
-  Array.iteri (fun i line ->
-    for j = 0 to String.length line - 1 do
+  Array.iteri (fun i line -> 
+    for j = 0 to String.length line - 1 do    
       if i = cursor.y && j = cursor.x then
         (* draw the cursor in red *) 
-        if blink then draw_rectangle (10 + j * char_width) (10 + i * char_height) char_width char_height Color.red;
-
+        if blink then draw_rectangle (10 * line_offset + j * char_width) (10 + i * char_height) char_width char_height Color.red; 
+      
       (* draw a single character with given font*)
-      draw_text_ex font (String.make 1 line.[j]) (Vector2.create (10. +. float_of_int (j * char_width)) (10. +. float_of_int (i * char_height))) (float_of_int font_size) 1.0 Color.white;
+      draw_text_ex font (String.make 1 line.[j]) (Vector2.create (10. *. (float_of_int line_offset) +. float_of_int (j * char_width)) (10. +. float_of_int (i * char_height))) (float_of_int font_size) 1.0 Color.white;
     done
   ) buffer
 
 (* delete a single character in a line*)
 let remove_char_at_cursor buffer cursor = 
-  let line = buffer.(cursor.y) in
+  let x, y = !cursor.x, !cursor.y in 
+  let line = buffer.(y) in
   let len = String.length line in 
-  if cursor.x > len-1 then invalid_arg "index_out_of_bound" (* this should never happen*)
+  if x > len-1 then invalid_arg "index_out_of_bound" (* this should never happen*)
   else 
-    let i = cursor.x in 
-    if len == 1 
-      then buffer.(cursor.y) <- " " 
+    let i = x in 
+    if len == 1
+    then buffer.(y) <- " "
     else 
-    buffer.(cursor.y) <- (String.sub line 0 i) ^ (String.sub line (i+1) (len - i - 1))
+      if len - 1 == i
+      (* in this case cursor blink*) 
+      then buffer.(y) <- (cursor := move_left !cursor; cursor_moving := true; String.sub line 0 i)
+      else buffer.(y) <- (String.sub line 0 i) ^ (String.sub line (i+1) (len - i - 1))
 
+let is_moving_key keys = 
+  let rec maux acc = function
+  | k :: ks ->  maux (acc || is_key_pressed k) ks
+  | [] -> acc
+  in maux false keys  
+
+let movement_keys = Key.H :: Key.L :: Key.K :: Key.J :: Key.W :: []
 
 let rec loop () =
   if Raylib.window_should_close () then Raylib.close_window ()
   else
+    (* cursor blink*)
     let current_t = get_time () in
     if abs_float (current_t -. !start_blink_time) >= blink_delay then (
       blink := not !blink; 
       start_blink_time := current_t;  
     );
 
-    if is_key_pressed Key.H || is_key_pressed Key.L || is_key_pressed Key.K || is_key_pressed Key.J 
-      then (blink := true; start_blink_time := current_t;); 
+    if (is_moving_key movement_keys)
+      then cursor_moving := true;    
 
     (*movements *)
-    if is_key_pressed Key.H then cursor := move_left !cursor;
     if is_key_pressed Key.L then cursor := move_right !cursor buffer;
+    if is_key_pressed Key.H then cursor := move_left !cursor;
     if is_key_pressed Key.K then cursor := move_up !cursor buffer;
     if is_key_pressed Key.J then cursor := move_down !cursor buffer;
+    (* this is garanteed working at all*)
+    if is_key_pressed Key.W then cursor := next_word_start !cursor buffer;
 
     (* editing*)
-    if is_key_pressed Key.X then remove_char_at_cursor buffer !cursor;
+    if is_key_pressed Key.X then remove_char_at_cursor buffer cursor;
+
+    if !cursor_moving then ((blink := true; start_blink_time := current_t;); cursor_moving := false); 
 
     begin_drawing ();
     clear_background Color.black;
 
     let font = load_font "resources/DejaVuSansMono.ttf" in 
     draw_buffer buffer !cursor font !blink;
+    draw_line_numbers buffer font;
 
     end_drawing ();
     loop ()
--- lib/cursor.ml
+++ lib/cursor.ml
@@ -23,3 +23,43 @@ let move_down cursor buffer =
     {y = m; x = min cursor.x (String.length (buffer.(cursor.y + 1)) - 1)}
   else
     {cursor with y = m}
+
+(* refactor in new module?? *)
+let is_alphanumeric = function 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9'-> true | _ -> false
+let is_not_alphanumeric c = not (is_alphanumeric c) && c != ' '
+
+(* TODO refactor this every method is the same change only the bool with a fun*)
+(** skip white spaces starting from i char and return the first non white space idx*)
+let rec skip_white_spaces line i = 
+  if i >= String.length line then String.length line - 1
+  else if line.[i] = ' ' then skip_white_spaces line (i+1) 
+  else (print_string ("skip white "); print_char line.[i];print_int i;print_endline ""; i)  
+
+let rec skip_alphanumeric line i =
+  if i >= String.length line then String.length line - 1 
+  else if is_alphanumeric line.[i] then skip_alphanumeric line (i+1) 
+  else (print_string "skip alpha "; print_int i; print_endline ""; i)
+
+let rec skip_non_alphanumeric line i = 
+  if i >= String.length line then String.length line - 1 
+  else if is_not_alphanumeric line.[i] then skip_non_alphanumeric line (i+1) 
+  else (print_string "skip non alpha "; print_int i;print_endline ""; i)
+
+let next_word_start cursor buffer =
+  let x, y= cursor.x, cursor.y in 
+  let line = buffer.(y) in
+  let i_start = skip_white_spaces line x in
+  let newx = 
+    if is_alphanumeric line.[i_start] 
+    then let newi = (skip_alphanumeric line i_start) in 
+      if is_not_alphanumeric line.[newi] then newi else skip_white_spaces line newi
+    else let newi = (skip_non_alphanumeric line i_start) in 
+      if is_alphanumeric line.[newi] then newi else skip_white_spaces line newi in
+  {cursor with x = newx}
+
+   
+(*TODO PROBLEM : cant go on single char words*)
+
+(*!**a asfafs*)
+
+(*Hello, world! afjajfj alora.mag0num(dei) , . gnu.,_gnu !&(;%."£",faffas*)
